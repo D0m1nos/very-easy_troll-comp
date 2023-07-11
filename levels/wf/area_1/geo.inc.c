04c55f7ca622432693831039673a9693cc8f08cc
D0m1nos <21263344+D0m1nos@users.noreply.github.com>
2023-07-11 19:00:41 +0200

    wf

--- levels/wf/area_1/geo.inc.c
+++ levels/wf/area_1/geo.inc.c
@@ -3,9 +3,7 @@
 const GeoLayout wf_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 28, 73, -37, wf_dl_Cube_mesh_layer_1),
-		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 1499, -678, -35, wf_dl_Cube_001_mesh_layer_1),
-		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 587, -304, -36, wf_dl_Cube_002_mesh_layer_1),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 28, 17, -37, wf_dl_Cylinder_mesh_layer_1),
 		GEO_TRANSLATE_NODE_WITH_DL(LAYER_TRANSPARENT, 28, -4587, -37, wf_dl_Plane_mesh_layer_5),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
